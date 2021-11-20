7e980af0b6e6c24e4e57141879cb4320f8178fa6
akash5852 <shara98@mcmaster.ca>
2021-11-20 16:29:39 -0500

    added front end and finished some routes

--- server/routes/meals.js
+++ server/routes/meals.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const mealRoutes = express.Router();
+const router = express.Router();
 
 let Meal = require("../meal.model");
 
-mealRoutes.route("/meal").get((req,res)=>{
-    
+// get all meals
+router.route("/").get(async (req,res) =>{
+    const meals = await Meal.find({})
+    res.send(Meals)
 })
+
+router.route("/addMeal").post(async (req,res) =>{
+    var willAddMeal =  new Meal({});
+    willAddMeal.name = req.body.name;
+    willAddMeal.mealType = req.body.mealType;
+    willAddMeal.save(function (err) {
+        if (err) return console.error(err);
+    });
+})
+
+router.route("/randomMeal").get(async (req,res) =>{
+    Meal.find({ meal_type: req.query.meal_type}, function (err, docs) {
+        var result = docs.map(function(doc) {return doc.meal_name;});
+        res.send(result[Math.random() * result.length])
+        if (err) return console.error(err);
+    });
+})
+
+module.exports = router
