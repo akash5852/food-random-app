ac5713529a9e16a5c65be9aa3229e6445a61d376
akash5852 <shara98@mcmaster.ca>
2021-11-22 17:33:30 -0500

    m

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router,Routes , Route, Link } from "react-router-dom";
+import { BrowserRouter as Router,Route, Link } from "react-router-dom";
 
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import MealsList from "./components/list.component";
-import RandomMeal from "./components/random.component";
 import CreateMeal from "./components/create.component";
 
 class App extends Component {
@@ -14,12 +12,9 @@ class App extends Component {
       <Router>
          <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <Link to="/" className="navbar-brand">Random Meal App</Link>
             <div className="collpase navbar-collapse">
               <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/randomMeal" className="nav-link">Todos</Link>
-                </li>
                 <li className="navbar-item">
                   <Link to="/addMeal" className="nav-link">Create Todo</Link>
                 </li>
@@ -28,11 +23,8 @@ class App extends Component {
           </nav>
           <br/>
           
-        <Routes>
-        <Route path="/" exact component={MealsList} />
-        <Route path="/randomMeal" component={RandomMeal} />
+        
         <Route path="/addMeal" component={CreateMeal} />
-        </Routes>
         </div>
         
       </Router>
