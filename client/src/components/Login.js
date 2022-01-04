ddf4343678e65558eae6cf41b2390c85404f0a8a
Akash Sharma <shara98@mcmaster.ca>
2022-01-04 13:42:25 -0500

    login change v1

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,9 @@
-import { useHistory } from "react-router";
-import { useEffect } from "react";
+import { useLayoutEffect, useState  } from 'react';
+import { Link, Redirect, useHistory } from 'react-router-dom'
 import axios from "axios";
 
 const Login = () => {
+    const [errorMessage, setErrorMessage] = useState("")
     const history = useHistory();
 
     const handleLogin = async (e) => {
@@ -24,7 +25,7 @@ const Login = () => {
 
     }
     
-    useEffect(() => {
+    useLayoutEffect(() => {
         fetch("/isUserAuth", {
             headers: {
                 "access-token": localStorage.getItem("token")
@@ -32,14 +33,23 @@ const Login = () => {
         })
         .then(res => res.json())
         .then(data => data.isLoggedIn ? history.push("/"): null)
-    }, [])
+        .catch(err => setErrorMessage(err)) 
+    }, [history])
 
     return (
-        <form onSubmit={e => handleLogin(e)}>
-            <input required />
-            <input required type ="password"/>
-            <input type = "submit" value= "Subimt"/>
-        </form>
+        <div className="text-white flex flex-col h-screen w-screen items-center justify-center">
+        <div className="p-5 text-3xl font-extrabold">Login</div>
+        <form className="mx-5 flex flex-col w-72" onSubmit={(e) => handleLogin(e)}>
+            <label htmlFor="username">Username</label>
+            <input className="text-black m-3 border-2 border-green-400 p-1" type="text" name="username" id="username" />
+            <label htmlFor="password">Password</label>
+            <input className="text-black m-3 border-2 border-green-400 p-1" type="password" name="password" id="password" />
+            <input className="m-1 px-2 py-1 rounded font-bold text-xl bg-green-400 text-gray-900" type="submit" value="Login"/>
+            <div className="flex flex-row items-center justify-center">
+                <h1>Don't have an account?</h1>
+                <Link className="m-1 px-2 py-1 rounded font-bold text-xl border-2 border-green-400 text-green-400 text-center" to="/register">Register</Link>
+            </div>            </form>
+    </div>
     )
 
 
