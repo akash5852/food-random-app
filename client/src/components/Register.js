68bd27fa3f95baae06c66275f84c98a9b9ca8d83
Akash Sharma <shara98@mcmaster.ca>
2022-01-04 20:08:36 -0500

    m

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,7 @@
 import { useLayoutEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom'
 import axios from "axios";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const Register = () => {
     const [errorMessage, setErrorMessage] = useState("")
     const history = useHistory();
@@ -15,7 +15,7 @@ const Register = () => {
         }
         try {
             await axios.post("http://localhost:5000/register", user);
-            
+
         } catch (e) {
             console.log(e);
         }
@@ -34,20 +34,27 @@ const Register = () => {
     }, [history])
 
 
-  
+
 
     return (
         <div className="text-white flex flex-col h-screen w-screen items-center justify-center">
-            <div className="p-5 text-3xl font-extrabold">Register</div>
-            <form className="mx-5 flex flex-col w-72" onSubmit={(e) => handleRegister(e)}>
-                <label htmlFor="username">Username</label>
-                <input className="text-black m-3 border-2 border-green-400 p-1" type="text" name="username" id="username" />
-                <label htmlFor="password">Password</label>
-                <input className="text-black m-3 border-2 border-green-400 p-1" type="password" name="password" id="password" />
-                <input className="m-1 px-2 py-1 rounded font-bold text-xl bg-green-400 text-gray-900" type="submit" value="Register" />
-                <div className="flex flex-row items-center justify-center">
-                    <h1>Already have an account?</h1>
-                    <Link className="m-1 px-2 py-1 rounded font-bold text-xl border-2 border-green-400 text-green-400 text-center" to="/login">Login</Link>
+            <div className="h2 p-5  ">Register</div>
+            <form className="mx-5 " onSubmit={(e) => handleRegister(e)}>
+                <div className='d-block'>
+                <label required className="d-inline  m-2" htmlFor="username">Username: </label>
+                <input required className="d-inline m-2" type="text" name="username" id="username" />
+                <label required className="d-inline m-2" htmlFor="password">Password: </label>
+                <input required  className="d-inline  m-2" type="password" name="password" id="password" />
+                <input className="m-1 px-2 py-1 " type="submit" value="Register" />
+                </div>
+               
+                <div className="justify-content-center">
+                <div className='d-block'>
+                    <p className = "h3 pt-5">Already have an account? </p>
+                    </div>
+                    <div className='d-block'>
+                    <Link className="h3 pt-5" to="/login">Login</Link>
+                    </div>
                 </div>            </form>
         </div>
     )
